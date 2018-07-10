54b431faa2252de9ddc9887906b2df1d74643074
Carter Cook <13cjcc@gmail.com>
2018-07-09 23:28:07 -0400

    Imported TypeSafety

---
+++ Assets/Standard Assets/StompyRobot/TypeSafe/usr/AudioMixers.Generated.cs
@@ -0,0 +1,25 @@
+// ------------------------------------------------------------------------------
+//  _______   _____ ___ ___   _   ___ ___ 
+// |_   _\ \ / / _ \ __/ __| /_\ | __| __|
+//   | |  \ V /|  _/ _|\__ \/ _ \| _|| _| 
+//   |_|   |_| |_| |___|___/_/ \_\_| |___|
+// 
+// This file has been generated automatically by TypeSafe.
+// Any changes to this file may be lost when it is regenerated.
+// https://www.stompyrobot.uk/tools/typesafe
+// 
+// TypeSafe Version: 1.3.2-Unity5
+// 
+// ------------------------------------------------------------------------------
+
+namespace TypeSafety {
+    
+    
+    public sealed class AudioMixers {
+        
+        private AudioMixers() {
+        }
+        
+        private const string _tsInternal = "1.3.2-Unity5";
+    }
+}
---
+++ Assets/Standard Assets/StompyRobot/TypeSafe/usr/Scenes.Generated.cs
@@ -0,0 +1,33 @@
+// ------------------------------------------------------------------------------
+//  _______   _____ ___ ___   _   ___ ___ 
+// |_   _\ \ / / _ \ __/ __| /_\ | __| __|
+//   | |  \ V /|  _/ _|\__ \/ _ \| _|| _| 
+//   |_|   |_| |_| |___|___/_/ \_\_| |___|
+// 
+// This file has been generated automatically by TypeSafe.
+// Any changes to this file may be lost when it is regenerated.
+// https://www.stompyrobot.uk/tools/typesafe
+// 
+// TypeSafe Version: 1.3.2-Unity5
+// 
+// ------------------------------------------------------------------------------
+
+namespace TypeSafety {
+    
+    
+    public sealed class Scenes {
+        
+        private Scenes() {
+        }
+        
+        private const string _tsInternal = "1.3.2-Unity5";
+        
+        private static global::System.Collections.Generic.IList<global::TypeSafe.Scene> __all = new global::System.Collections.ObjectModel.ReadOnlyCollection<global::TypeSafe.Scene>(new global::TypeSafe.Scene[0]);
+        
+        public static global::System.Collections.Generic.IList<global::TypeSafe.Scene> All {
+            get {
+                return __all;
+            }
+        }
+    }
+}
