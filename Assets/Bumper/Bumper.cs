1c5ff5de95c15ef7e92b66e8ac4c34678a0627fd
Carter Cook <13cjcc@gmail.com>
2018-07-09 23:24:53 -0400

    Bumper Skeleton

---
+++ Assets/Bumper/Bumper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class Bumper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Vector3 averageNormals = other.contacts.Select(x => x.normal).Aggregate((a, b) => a + b) / other.contacts.Length;
+
+		Rigidbody2D otherBody = other.gameObject.GetComponent<Rigidbody2D>();
+
+		if (otherBody != null)
+		{
+			otherBody.AddForce(averageNormals, ForceMode2D.Impulse);
+		}
+	}
+}
