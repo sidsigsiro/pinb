84029adc9d09b9161da089c545eadba7a21997eb
Carter Cook <13cjcc@gmail.com>
2018-07-31 20:29:55 -0400

    replaced Anima2D and Dragonbones with Spine plugin

--- Assets/Standard Assets/StompyRobot/TypeSafe/usr/Resources.Generated.cs
+++ Assets/Standard Assets/StompyRobot/TypeSafe/usr/Resources.Generated.cs
@@ -24,88 +24,6 @@ namespace TypeSafety {
         
         private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_resources = new global::System.Collections.ObjectModel.ReadOnlyCollection<global::TypeSafe.IResource>(new global::TypeSafe.IResource[0]);
         
-        public sealed class Shaders {
-            
-            private Shaders() {
-            }
-            
-            public static global::TypeSafe.Resource<global::UnityEngine.Shader> DB_BlendMode_Grab {
-                get {
-                    return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[0]));
-                }
-            }
-            
-            public static global::TypeSafe.Resource<global::UnityEngine.Shader> DB_BlendMode_UIGrab {
-                get {
-                    return ((global::TypeSafe.Resource<global::UnityEngine.Shader>)(__ts_internal_resources[1]));
-                }
-            }
-            
-            private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_resources = new global::System.Collections.ObjectModel.ReadOnlyCollection<global::TypeSafe.IResource>(new global::TypeSafe.IResource[] {
-                        new global::TypeSafe.Resource<global::UnityEngine.Shader>("DB_BlendMode_Grab", "Shaders/DB_BlendMode_Grab"),
-                        new global::TypeSafe.Resource<global::UnityEngine.Shader>("DB_BlendMode_UIGrab", "Shaders/DB_BlendMode_UIGrab")});
-            
-            /// <summary>
-            /// Return a list of all resources in this folder.
-            /// This method has a very low performance cost, no need to cache the result.
-            /// </summary>
-            /// <returns>A list of resource objects in this folder.</returns>
-            public static global::System.Collections.Generic.IList<global::TypeSafe.IResource> GetContents() {
-                return __ts_internal_resources;
-            }
-            
-            private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_recursiveLookupCache;
-            
-            /// <summary>
-            /// Return a list of all resources in this folder and all sub-folders.
-            /// The result of this method is cached, so subsequent calls will have very low performance cost.
-            /// </summary>
-            /// <returns>A list of resource objects in this folder and sub-folders.</returns>
-            public static global::System.Collections.Generic.IList<global::TypeSafe.IResource> GetContentsRecursive() {
-                if ((__ts_internal_recursiveLookupCache != null)) {
-                    return __ts_internal_recursiveLookupCache;
-                }
-                global::System.Collections.Generic.List<global::TypeSafe.IResource> tmp = new global::System.Collections.Generic.List<global::TypeSafe.IResource>();
-                tmp.AddRange(GetContents());
-                __ts_internal_recursiveLookupCache = tmp;
-                return __ts_internal_recursiveLookupCache;
-            }
-            
-            /// <summary>
-            /// Return a list of all resources in this folder of type <typeparamref>TResource</typeparamref> (does not include sub-folders)
-            /// This method does not cache the result, so you should cache the result yourself if you will use it often.
-            /// </summary>
-            /// <returns>A list of <typeparamref>TResource</typeparamref> objects in this folder.</returns>
-            public static global::System.Collections.Generic.List<global::TypeSafe.Resource<TResource>> GetContents<TResource>()
-                where TResource : global::UnityEngine.Object {
-                return global::TypeSafe.TypeSafeUtil.GetResourcesOfType<TResource>(GetContents());
-            }
-            
-            /// <summary>
-            /// Return a list of all resources in this folder of type <typeparamref>TResource</typeparamref>, including sub-folders.
-            /// This method does not cache the result, so you should cache the result yourself if you will use it often.
-            /// </summary>
-            /// <returns>A list of <typeparamref>TResource</typeparamref> objects in this folder and sub-folders.</returns>
-            public static global::System.Collections.Generic.List<global::TypeSafe.Resource<TResource>> GetContentsRecursive<TResource>()
-                where TResource : global::UnityEngine.Object {
-                return global::TypeSafe.TypeSafeUtil.GetResourcesOfType<TResource>(GetContentsRecursive());
-            }
-            
-            /// <summary>
-            /// Call Unload() on every loaded resource in this folder.
-            /// </summary>
-            public static void UnloadAll() {
-                global::TypeSafe.TypeSafeUtil.UnloadAll(GetContents());
-            }
-            
-            /// <summary>
-            /// Call Unload() on every loaded resource in this folder and subfolders.
-            /// </summary>
-            private void UnloadAllRecursive() {
-                global::TypeSafe.TypeSafeUtil.UnloadAll(GetContentsRecursive());
-            }
-        }
-        
         /// <summary>
         /// Return a list of all resources in this folder.
         /// This method has a very low performance cost, no need to cache the result.
@@ -128,7 +46,6 @@ namespace TypeSafety {
             }
             global::System.Collections.Generic.List<global::TypeSafe.IResource> tmp = new global::System.Collections.Generic.List<global::TypeSafe.IResource>();
             tmp.AddRange(GetContents());
-            tmp.AddRange(Shaders.GetContentsRecursive());
             __ts_internal_recursiveLookupCache = tmp;
             return __ts_internal_recursiveLookupCache;
         }
