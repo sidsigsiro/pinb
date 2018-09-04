ee4e6ef271fd0c0493c2d7c5eff6be0d19ef29e7
carter-cook <carter.cook@prodigygame.com>
2018-09-03 21:43:26 -0400

    Today I made the bumper work and also dash is waaay better. Also you bounce off the walls.

--- Assets/Bumper/Bumper.cs
+++ Assets/Bumper/Bumper.cs
@@ -5,25 +5,10 @@ using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class Bumper : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void OnCollisionEnter(Collision other)
-	{
-		Vector3 averageNormals = other.contacts.Select(x => x.normal).Aggregate((a, b) => a + b) / other.contacts.Length;
-
-		Rigidbody2D otherBody = other.gameObject.GetComponent<Rigidbody2D>();
-
-		otherBody.AddForce(averageNormals, ForceMode2D.Impulse);
-		
-		print("hi");
-	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Vector2 averageNormals = other.contacts.Select(x => x.normal).Aggregate((a, b) => a + b) / other.contacts.Length;
+        Rigidbody2D otherBody = other.gameObject.GetComponent<Rigidbody2D>();
+        otherBody.AddForce(-averageNormals, ForceMode2D.Impulse);
+    }
 }
